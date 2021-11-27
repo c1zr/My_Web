96809576d85cf0f6ced875132eac5fefdbdad77f
c1zr <jendacizu@email.cz>
2021-11-27 16:33:08 +0100

    Zmena struktury adresare

---
+++ My_web/omne.php
@@ -0,0 +1,50 @@
+<?php include "_partials/header.php" ?>
+</head>
+<body class='body-side-2'>
+
+    <header class="header-side-2">
+        <h1>O mně</h1>
+        <hr>
+    </header>
+
+    <main class="main-side-2">
+        <article>
+           <h2>In progress</h2><br>
+           <p>HTML, CSS, PHP, MySQL, Laravel, JavaScript...</p> 
+
+           <button class="btn btn-primary" type="button" disabled>
+            <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+            Loading skills.
+           </button>
+        </article>
+
+        <nav class="nav-side-2">
+            <h2>Kontakt</h2>
+            <ul class='ul-side-2'>
+                <li><a href="mailto:jendacizu@email.cz">💬 jendacizu@ email.cz</a></li><br /><br />
+                <li><a href="tel:773-080-980">📞 +420 773 080 980</a></li>
+            </ul>
+        </nav>
+
+        <aside>
+           <a href="#" target="_blank"><img class="scaled" src="img/github2.png" width="300" alt="GitHub">GitHub</a>
+        </aside>
+
+        <aside>
+           <a href="Ciz_Zivotopis.pdf" target="_blank"><img class="scaled" src="img/cv1.png" width="300" alt="CV">Životopis</a>
+        </aside>
+    
+    </main>
+
+    <h3 class="bydlim">Tady bydlím<br><hr>
+        🌳🏡🏡🏡🌳
+    </h3>
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2581.590629569603!2d14.000359315875327!3d49.68084945039721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b0e127e23e221%3A0x1422c7238b43adc0!2zdMWZLiBPc3ZvYm96ZW7DrSwgMjYxIDAxIFDFmcOtYnJhbQ!5e0!3m2!1scs!2scz!4v1636793259557!5m2!1scs!2scz" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+    <footer>
+        Tento web vytvořil můj 🐕 Matýsek. FYI.
+    </footer>
+
+</body>
+</html>
---
+++ My_web/ukazka.php
@@ -0,0 +1,16 @@
+<?php include "_partials/header.php" ?>
+<body>
+
+    <div class="bgimg">
+        
+        <div class="middle">
+          <h1>COMING SOON</h1>
+            <hr>
+                <p>
+                    Only a few days left 
+                </p>
+        </div>
+    </div>
+    
+</body>
+</html>
